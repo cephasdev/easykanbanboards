a7577de0a8cfbd5fe12be59e116627c0ba466c2c
DESKTOP-QTNUKMU\hp <me@cephasdev.space>
2025-01-12 20:55:48 +0100

    Add: tests for components. [wip]

--- client/src/components/Card/Card.test.tsx
+++ client/src/components/Card/Card.test.tsx
@@ -26,3 +26,76 @@ describe('Card Component', () => {
     expect(el).toBeInTheDocument();
   });
 });
+
+test('when the card is double clicked, the onDoubleClick function should be called', () => {
+  // Arrange
+  const onDoubleClick = jest.fn();
+  const onCardCloseClicked = jest.fn();
+  const cardTitle = 'Test card title';
+  // Act
+  render(
+    <ThemeProvider theme={theme}>
+      <Card
+        title={cardTitle}
+        cardId={''}
+        onDoubleClick={onDoubleClick}
+        onCardCloseClicked={onCardCloseClicked}
+      />
+    </ThemeProvider>,
+  );
+  // Assert
+  const el = screen.getByText(cardTitle);
+  el.dispatchEvent(new MouseEvent('dblclick', { bubbles: true }));
+  expect(onDoubleClick).toHaveBeenCalled();
+});
+
+test('when the close button is clicked, the onCardCloseClicked function should be called', () => {
+  // Arrange
+  const onDoubleClick = jest.fn();
+  const onCardCloseClicked = jest.fn();
+  const cardTitle = 'Test card title';
+  // Act
+  render(
+    <ThemeProvider theme={theme}>
+      <Card
+        title={cardTitle}
+        cardId={''}
+        onDoubleClick={onDoubleClick}
+        onCardCloseClicked={onCardCloseClicked}
+      />
+    </ThemeProvider>,
+  );
+  // Assert
+  const closeButton = screen.getByLabelText('close card');
+  closeButton.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
+  expect(onCardCloseClicked).toHaveBeenCalled();
+});
+
+test('should apply transform style when card is dragged', () => {
+  // Arrange
+  const onDoubleClick = jest.fn();
+  const onCardCloseClicked = jest.fn();
+  const cardTitle = 'Test card title';
+  const cardId = 'test-card-id';
+  // Act
+  render(
+    <ThemeProvider theme={theme}>
+      <Card
+        title={cardTitle}
+        cardId={cardId}
+        onDoubleClick={onDoubleClick}
+        onCardCloseClicked={onCardCloseClicked}
+      />
+    </ThemeProvider>,
+  );
+  // Assert
+  const card = screen.getByText(cardTitle).parentElement;
+  console.log({ card: card?.style });
+  // if (card) {
+  //   fireEvent.mouseDown(card);
+  //   expect(card).toHaveStyle('transform: translate(0px, 0px)');
+  //   // fireEvent.mouseUp(card);
+  // }
+  card?.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
+  expect(card).toHaveStyle('transform: translate(0px, 0px)');
+});
