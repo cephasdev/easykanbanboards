158a826c51a5c77728b5a36220df38d3915dffd7
DESKTOP-QTNUKMU\hp <me@cephasdev.space>
2025-01-15 23:48:17 +0100

    Code cleanup.

--- client/src/state/api/api-slice.ts
+++ client/src/state/api/api-slice.ts
@@ -18,7 +18,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/graphql' }),
-  // tagTypes: ['Boards', 'Cards', 'Users'],
   tagTypes: ['Cards', 'Users'],
   // cards
   endpoints: (builder) => ({
@@ -39,10 +38,6 @@ export const apiSlice = createApi({
         },
       }),
       providesTags: ['Cards'],
-      //   providesTags: (result) =>
-      //     result
-      //       ? result.items.map(({ id }: { id: string }) => ({ type: 'Item', id }))
-      //       : ['Item'],
     }),
     addCard: builder.mutation<ICardAddResponse, ICardAddInput>({
       query: (card) => ({
